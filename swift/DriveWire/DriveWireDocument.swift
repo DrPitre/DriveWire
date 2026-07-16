0503dacd4f599d6daca0df7c72b043eec17a0770
Boisy Gene Pitre <github@pitre.org>
2026-07-16 09:27:38 -0500

    Refine dashboard layout and restore serial reconnect

--- swift/DriveWire/DriveWireDocument.swift
+++ swift/DriveWire/DriveWireDocument.swift
@@ -21,10 +21,44 @@ final class DriveWireDocument: FileDocument {
         var serialDriver: DriveWireSerialDriver
         var tcpDriver: DriveWireTCPDriver
         var connectionType: ConnectionType
+        var detailedOpcodeLogging: Bool
+
+        init(
+            serialDriver: DriveWireSerialDriver,
+            tcpDriver: DriveWireTCPDriver,
+            connectionType: ConnectionType,
+            detailedOpcodeLogging: Bool = false
+        ) {
+            self.serialDriver = serialDriver
+            self.tcpDriver = tcpDriver
+            self.connectionType = connectionType
+            self.detailedOpcodeLogging = detailedOpcodeLogging
+        }
+
+        init(from decoder: Decoder) throws {
+            let container = try decoder.container(keyedBy: CodingKeys.self)
+            serialDriver = try container.decode(DriveWireSerialDriver.self, forKey: .serialDriver)
+            tcpDriver = try container.decode(DriveWireTCPDriver.self, forKey: .tcpDriver)
+            connectionType = try container.decode(ConnectionType.self, forKey: .connectionType)
+            detailedOpcodeLogging = try container.decodeIfPresent(Bool.self, forKey: .detailedOpcodeLogging) ?? false
+        }
     }
     
-    @Published var serialDriver = DriveWireSerialDriver()
-    @Published var tcpDriver = DriveWireTCPDriver()
+    @Published var serialDriver = DriveWireSerialDriver() {
+        didSet {
+            applyLoggingPreferences()
+        }
+    }
+    @Published var tcpDriver = DriveWireTCPDriver() {
+        didSet {
+            applyLoggingPreferences()
+        }
+    }
+    @Published var detailedOpcodeLogging = false {
+        didSet {
+            applyLoggingPreferences()
+        }
+    }
     
     enum ConnectionType: String, CaseIterable, Identifiable, Codable {
         case serial, network
@@ -35,7 +69,14 @@ final class DriveWireDocument: FileDocument {
     
     static var readableContentTypes: [UTType] { [.exampleText] }
     
-    init() {
+    init(connectionType: ConnectionType = .serial) {
+        self.connectionType = connectionType
+        applyLoggingPreferences()
+    }
+
+    private func applyLoggingPreferences() {
+        serialDriver.host.detailedOpcodeLogging = detailedOpcodeLogging
+        tcpDriver.host.detailedOpcodeLogging = detailedOpcodeLogging
     }
     
     struct ReloadVirtualDriveIntent: AppIntent {
@@ -76,16 +117,42 @@ final class DriveWireDocument: FileDocument {
         self.serialDriver = model.serialDriver
         self.tcpDriver = model.tcpDriver
         self.connectionType = model.connectionType
+        self.detailedOpcodeLogging = model.detailedOpcodeLogging
+        applyLoggingPreferences()
+        if connectionType == .serial {
+            serialDriver.restoreConnectionIfNeeded()
+        }
     }
     
     func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
         let model = DriveWireDocumentModel(
             serialDriver: self.serialDriver,
             tcpDriver: self.tcpDriver,
-            connectionType: self.connectionType
+            connectionType: self.connectionType,
+            detailedOpcodeLogging: self.detailedOpcodeLogging
         )
         
         let data = try JSONEncoder().encode(model)
         return .init(regularFileWithContents: data)
+    }
+
+    func persistCurrentState(to fileURL: URL?) {
+        guard let fileURL else {
+            return
+        }
+
+        let model = DriveWireDocumentModel(
+            serialDriver: self.serialDriver,
+            tcpDriver: self.tcpDriver,
+            connectionType: self.connectionType,
+            detailedOpcodeLogging: self.detailedOpcodeLogging
+        )
+
+        do {
+            let data = try JSONEncoder().encode(model)
+            try data.write(to: fileURL, options: .atomic)
+        } catch {
+            serialDriver.log += "Failed to save document state: \(error.localizedDescription)\n"
+        }
     }
 }
